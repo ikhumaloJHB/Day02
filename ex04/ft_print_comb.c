633181c065cd5194c4cfbd73393819f73d076cfc
Innocent Khumalo <ikhumalo@c5r4s5.wethinkcode.co.za>
2020-06-20 09:49:09 +0200

    Print Combo

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: ikhumalo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 17:05:20 by ikhumalo          #+#    #+#             */
-/*   Updated: 2020/06/19 17:50:44 by ikhumalo         ###   ########.fr       */
+/*   Updated: 2020/06/20 09:44:26 by ikhumalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,31 @@ int ft_print_comb(void){
 	int m;
 	int num[10];
 	int temp;
-	int t =3;	
+	int t =4;	
 
 
 	
         
-		num[1]=1;
-		num[2]=3
-		num[3]=7	
+		num[0]=1;
+		num[1]=3;
+		num[2]=7;	
 			
- while
+       while(l<=t){
+		   while(m<=t-1){
+			   temp = num[m];
+			   num[m] = num[m+1];
+			   num[m+1] = temp;
+	            write(1,&num[m],1);
+	              m++;
+	        };
+            l++;
+	    };
+
+	   return 0;
+};
+
+int main(){
+
+  ft_print_comb();
+  return 0;
+};
